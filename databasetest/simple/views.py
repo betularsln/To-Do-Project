ad3e883c83dd3376c5547c52a00abbd02432806f
betularsln <betularslanbb@gmail.com>
2020-09-27 01:31:13 +0300

    view' e login eklendi
    view' e login eklendi

--- databasetest/simple/views.py
+++ databasetest/simple/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Todo
 from .forms import ListForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -57,5 +59,34 @@ def update(request,Todo_id):
     else:
         todo_list = Todo.objects.all()
         return render(request,"simple/update.html",{'todo_list':todo_list})
+
+
+def user_login(request):    	
+    if request.method == 'POST':
+        # First get the username and password supplied
+        username = request.POST.get('username')
+        password = request.POST.get('password')
     
+        # Django's built-in authentication function:
+        user = authenticate(username=username, password=password)
     
+        # If we have a user
+        if user:
+            #Check it the account is active
+            if user.is_active:
+                # Log the user in.
+                # login(request,user)
+                # Send the user back to some page.
+                # In this case their homepage.
+                return HttpResponseRedirect('index')
+            else:
+                # If account is not active:
+                return HttpResponse("Your account is not active.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details supplied.")
+    
+    else:
+        #Nothing has been provided for username or password.
+        return render(request, 'simple/login.html', {})
